b6b593503b052cc127ca4504988bd20c1f824090
thecristen <cristenblair@gmail.com>
2019-04-07 10:43:18 -0400

    add a datatable

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
     
     // download data
-    d3.csv("data/mpm_data.csv", function(data) {
-        console.log(data);
+    // d3.csv("data/mpm_data.csv").then(function(data) {
+    d3.csv("data/mpm_data.csv").then(function(data) {
+        // console.log(data);
+
+        var mpmCountyYear = d3.nest()
+            .key(function (d) { return d.county; }).sortKeys(d3.ascending)
+            .key(function (d) { return d.year; }).sortKeys(d3.ascending)
+            .entries(data);
+
+        console.log(mpmCountyYear)
+        console.table(data[0])
+
+        $('#mpmTable').DataTable({
+            data: data,
+            "columns": [
+                { "data": "county" },
+                { "data": "year" },
+                { "data": "federal_supply_category_name" },
+                { "data": "quantity" },
+                { "data": "total_cost" },
+            ]
+        });
     });
+
+    
 });
